7611bfb7992c8a64b4528f3daea27262e9ddd937
Harry <harryfinkhynes@gmail.com>
2024-04-20 16:44:45 -0400

    First commit: Set up a Django project with tailwind integrated. Created the backend for a social media app (Users, Profiles...)

---
+++ social/hotdogger/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+            "self",
+            related_name="followed_by",
+            symmetrical=False,
+            blank=True
+        )
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User) # Signal to create a profile when a user is created
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile) # Foloow yourself
+        user_profile.save()
+
+
+
