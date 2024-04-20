5eea626772d9254cfc53567f31376ea24cf76f67
Harry <harryfinkhynes@gmail.com>
2024-04-20 18:42:54 -0400

    Added Profile list and profile pages

--- social/hotdogger/views.py
+++ social/hotdogger/views.py
@@ -1,1 +1,18 @@
 from django.shortcuts import render
+from .models import Profile
+
+def dashboard(request):
+    return render(request, "base.html")
+
+def profile_list(request):
+    if request.user.is_anonymous:
+         profiles = Profile.objects.all()
+    else:
+          profiles = Profile.objects.exclude(user=request.user.profile)
+        
+
+    return render(request, "hotdogger/profile_list.html", {"profiles": profiles})
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "hotdogger/profile.html", {"profile": profile})
--- social/social/urls.py
+++ social/social/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include,path
 from django.views.generic import TemplateView
 
+
  
 urlpatterns = [
+    path("", include("hotdogger.urls")),
     path('admin/', admin.site.urls),
     path("", TemplateView.as_view(template_name="base.html")),
     path("__reload__/", include("django_browser_reload.urls")),
