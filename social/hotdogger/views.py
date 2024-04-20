6231a9b40a5233e495eecf46e8120c2a7b047187
Harry <harryfinkhynes@gmail.com>
2024-04-20 19:16:36 -0400

    Added Follow/Unfollow button

--- social/hotdogger/views.py
+++ social/hotdogger/views.py
@@ -5,14 +5,29 @@ def dashboard(request):
     return render(request, "base.html")
 
 def profile_list(request):
+
     if request.user.is_anonymous:
          profiles = Profile.objects.all()
     else:
-          profiles = Profile.objects.exclude(user=request.user.profile)
+          profiles = Profile.objects.exclude(user=request.user)
+ 
         
 
     return render(request, "hotdogger/profile_list.html", {"profiles": profiles})
 
 def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
     profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        print(action)
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
     return render(request, "hotdogger/profile.html", {"profile": profile})
