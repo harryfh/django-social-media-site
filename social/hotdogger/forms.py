3b1a8b58263e36cd71ae2d6e9f8b303a294144a8
Harry <harryfinkhynes@gmail.com>
2024-04-23 01:13:12 -0400

    hotdogblogger is looking good

--- social/hotdogger/forms.py
+++ social/hotdogger/forms.py
@@ -2,14 +2,20 @@ from django import forms
 from .models import BlogPost
 
 class BlogForm(forms.ModelForm):
-
+    title = forms.CharField(required=True,         widget=forms.widgets.TextInput(
+            attrs={
+                "placeholder": "TITLE...",
+                "class": "rounded btn text-lg bg-yellow text-red w-full mb-3 jersey",
+            }
+        ),
+        label="",)
     content = forms.CharField(required=True,         widget=forms.widgets.Textarea(
             attrs={
-                "placeholder": "blog...",
-                "class": "rounded btn text-lg bg-yellow text-red jersey",
+                "placeholder": "BLOG...",
+                "class": "w-full rounded btn text-lg bg-yellow text-red jersey",
             }
         ),
         label="",)
     class Meta:
         model = BlogPost
-        exclude = ("user", "title",  )
+        exclude = ("user",  )
